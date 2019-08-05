14fe2f1462fb1ef77ccf2742723f5d33bc59bd25
74ultra <r.barnhill@protonmail.com>
2019-08-05 14:19:29 -0700

    MVP reached with a stretch goal

--- js/index.js
+++ js/index.js
@@ -40,3 +40,146 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+// Nav items
+
+let navTags = document.querySelectorAll('header nav a');
+
+let navText = Object.values(siteContent.nav);
+
+const navUpdater = () => {
+  let i = 0;
+  navTags.forEach(function(item){
+    item.textContent = navText[i];
+    item.style.color = 'green';
+    i++;
+  })
+}
+
+navUpdater();
+
+
+// Call to action
+
+let ctaHeader = document.querySelector('.cta-text h1');
+
+let ctaButton = document.querySelector('.cta-text button');
+
+let ctaImg = document.querySelector('#cta-img');
+
+ctaHeader.textContent = siteContent.cta.h1;
+
+ctaButton.textContent = siteContent.cta.button;
+
+ctaImg.setAttribute('src', siteContent.cta['img-src']);
+
+
+// Top Content
+
+let topHeaders = document.querySelectorAll('.top-content .text-content h4');
+
+topHeaders[0].textContent = siteContent["main-content"]["features-h4"];
+
+topHeaders[1].textContent = siteContent["main-content"]["about-h4"];
+
+
+let topParas = document.querySelectorAll('.top-content .text-content p');
+
+topParas[0].textContent = siteContent["main-content"]["features-content"];
+
+topParas[1].textContent = siteContent["main-content"]["about-content"];
+
+
+// Middle image
+
+let midImg = document.querySelector('.middle-img');
+
+midImg.setAttribute('src', siteContent["main-content"]["middle-img-src"]);
+
+
+// Bottom content
+
+let bottomHeaders = document.querySelectorAll('.bottom-content .text-content h4')
+
+bottomHeaders[0].textContent = siteContent["main-content"]["services-h4"];
+
+bottomHeaders[1].textContent = siteContent["main-content"]["product-h4"];
+
+bottomHeaders[2].textContent = siteContent["main-content"]["vision-h4"];
+
+
+let bottomParas = document.querySelectorAll('.bottom-content .text-content p')
+
+bottomParas[0].textContent = siteContent["main-content"]["services-content"];
+
+bottomParas[1].textContent = siteContent["main-content"]["product-content"];
+
+bottomParas[2].textContent = siteContent["main-content"]["vision-content"];
+
+
+// Contact
+
+let contactHeader = document.querySelector('.contact h4');
+
+contactHeader.textContent = siteContent.contact["contact-h4"];
+
+
+let contactParas = document.querySelectorAll('.contact p')
+
+let contactParasText = Object.values(siteContent.contact);
+
+const contactParasUpdater = () => {
+  let i = 1;
+  contactParas.forEach((item) => {
+    item.textContent = contactParasText[i];
+    i++;
+  })
+}
+
+contactParasUpdater();
+
+contactParas[0].innerHTML = '123 Way 456 Street<br>Somewhere, USA';
+
+
+// Footer
+
+let footerPara = document.querySelector('footer p');
+
+footerPara.textContent = siteContent.footer.copyright;
+
+
+// Append new nav item
+
+let appendedNavItem = document.createElement('a');
+
+let navBar = document.querySelector('header nav');
+
+appendedNavItem.textContent = "Consequences";
+
+appendedNavItem.style.color = 'green';
+
+navBar.appendChild(appendedNavItem);
+
+
+// Prepend new nav item
+
+let prependedNavItem = document.createElement('a');
+
+prependedNavItem.textContent = "Truth";
+
+prependedNavItem.style.color = "green";
+
+navBar.prepend(prependedNavItem);
+
+
+// Event listener on cta button to toggle - 'ctaButton' is already the variable name for the button on screen
+
+ctaButton.addEventListener('click', () => {
+  alert('You clicked the "Call To Action" button!')
+});
+
+
+
+
+
+
